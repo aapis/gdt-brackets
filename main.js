a62933260fd134d4ace27fbcd21566336ec78e52
Ryan Priebe <rpriebe@me.com>
2015-07-05 17:40:36 -0600

    basic scaffolding is done, now we just need a way to execute shell commands...

--- main.js
+++ main.js
@@ -7,35 +7,46 @@ define(function (require, exports, module) {
     var CommandManager = brackets.getModule("command/CommandManager"),
         Menus          = brackets.getModule("command/Menus");
 
-
-    // Github handler
-    function Github() {
-        alert('woop')
-        console.log("registering github handler")
+    var Helpers = {
+        // run_cmd: function(cmd, args, cb, end) {
+        //     var spawn = require('child_process').spawn,
+        //         child = spawn(cmd, args),
+        //         me = this;
+        //     child.stdout.on('data', function (buffer) { cb(me, buffer) });
+        //     child.stdout.on('end', end);
+        // },
+        derp: function(cmd, args){
+            brackets.shellAPI.executeCommand(cmd)
+        }
     }
 
-    // Recompile handler
-    function Recompile() {
-        console.log("registering recompile handler")
-    }
+    var Granify = {
+        // Github handler
+        Github: function(){
+            Helpers.derp("whoami")
+        },
 
-    // Run handler
-    function Run() {
-        console.log("registering run handler")
-    }
+        // Recompile handler
+        Recompile: function(){
+            console.log("registering recompile handler")
+        },
 
-    // Startup handler
-    function Startup() {
-        console.log("registering startup handler")
-    }
+        // Run handler
+        Run: function(){
+            console.log("registering run handler")
+        },
 
-    // Test handler
-    function Test() {
-        console.log("registering test handler")
-    }
+        // Startup handler
+        Startup: function(){
+            console.log("registering startup handler")
+        },
 
+        // Test handler
+        Test: function(){
+            console.log("registering test handler")
+        },
+    };
 
-    // First, register a command - a UI-less object associating an id to a handler
     var CMD_PREFIX = "granify.dev-tools.";   // package-style naming to avoid collisions
     var command_ids = [
         "Github",
@@ -46,13 +57,8 @@ define(function (require, exports, module) {
     ];
 
     for(var i = 0; i < command_ids.length; i++){
-        CommandManager.register(command_ids[i], CMD_PREFIX + command_ids[i], commands_ids[i]);
+        CommandManager.register(command_ids[i], CMD_PREFIX + command_ids[i], Granify[command_ids[i]]);
     }
-    // CommandManager.register("Github", MY_COMMAND_ID, github);
-    // CommandManager.register("Recompile", MY_COMMAND_ID, recompile);
-    // CommandManager.register("Run", MY_COMMAND_ID, run);
-    // CommandManager.register("Startup", MY_COMMAND_ID, startup);
-    // CommandManager.register("Test", MY_COMMAND_ID, test);
 
     // Then create a menu item bound to the command
     // The label of the menu item is the name we gave the command (see above)
@@ -62,8 +68,4 @@ define(function (require, exports, module) {
     for(var i = 0; i < command_ids.length; i++){
         menu.addMenuItem(CMD_PREFIX + command_ids[i]);
     }
-
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-H");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
 });
