ccd91a6961dd332b90449ac9f538c4cc6498b0dd
Ryan Priebe <rpriebe@me.com>
2015-07-05 20:06:38 -0600

    should be able to execute shell commands now

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ define(function (require, exports, module) {
     "use strict";
 
     var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
+        Menus          = brackets.getModule("command/Menus"),
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+        NodeDomain     = brackets.getModule("utils/NodeDomain");
 
     var Helpers = {
+        domain: new NodeDomain("simple", ExtensionUtils.getModulePath(module, "node/domain")),
         // run_cmd: function(cmd, args, cb, end) {
         //     var spawn = require('child_process').spawn,
         //         child = spawn(cmd, args),
@@ -16,14 +19,34 @@ define(function (require, exports, module) {
         //     child.stdout.on('end', end);
         // },
         derp: function(cmd, args){
-            brackets.shellAPI.executeCommand(cmd)
+            var COMMAND = "evertils",
+                ARGS = ["--version"];
+
+            var deferred = new $.Deferred(),
+            execPromise = this.domain.exec("execCommand", COMMAND, ARGS);
+
+            execPromise.then(function (result) {
+                var version = result.output;
+
+                console.log(version);
+                if (match) {
+                    deferred.resolve();
+                } else {
+                    deferred.reject();
+                }
+            }).fail(function (error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise();
         }
     }
 
     var Granify = {
         // Github handler
         Github: function(){
-            Helpers.derp("whoami")
+            console.log("should be derping");
+            Helpers.derp();
         },
 
         // Recompile handler
